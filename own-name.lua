a8b4f3b0745d57bbfc8e7316246fc3bf68077410
Jacobo Tibaquira <jacobotibaquira@gmail.com>
2013-10-30 01:15:18 -0500

    more scripts for examples
    Signed-off-by: Jacobo Tibaquira <jacobotibaquira@gmail.com>

---
+++ own-name.lua
@@ -0,0 +1,13 @@
+-- Script to print the own name
+
+print("Enter the name:")
+
+name = io.read()
+
+function ownname(name_captured)
+  print("Este es el nombre capturado: " .. name)
+end
+
+ownname(name)
+
+
